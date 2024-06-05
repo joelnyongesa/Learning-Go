42fc54f6ba13939990f5a07c176947e998340280
joelnyongesa <joelnyongesa148@gmail.com>
2024-06-05 14:00:07 +0300

    Lesson 2: Comments in Go

---
+++ 02- Comments/comments.go
@@ -0,0 +1,12 @@
+package main;
+import ("fmt");
+
+func main() {
+	// This is a single line comment
+	fmt.Println("Welcome to Comments in Go!");
+
+	/*
+	This is a multi-line comment
+	*/
+	fmt.Println("This comes after a multi-line comment in Go!")
+}
